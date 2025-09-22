bafa91a5cae99f1bfe6a654b518e8d46646adf62
varak <varaksinaalla@mail.ru>
2025-09-22 23:51:16 +0300

    add rss and markdown

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,7 +1,14 @@
 from django import template
+from django.utils.safestring import mark_safe
+import markdown
 from ..models import Post
 
 register = template.Library()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 @register.simple_tag
